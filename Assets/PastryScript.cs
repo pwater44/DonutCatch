ae0f9c6d75a6f5ed5dcde95117150f8dbb772526
pwater44 <56414116+pwater44@users.noreply.github.com>
2019-12-05 18:11:56 -0500

    First commit

---
+++ Assets/PastryScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PastryScript : MonoBehaviour
+{
+    [SerializeField] AudioClip BaRing;
+    [SerializeField] AudioClip missSound;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Entered");
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            AudioSource.PlayClipAtPoint(BaRing, transform.position);
+            GameManager.gm.incrementScore();
+
+            print("Enteredy");
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag.Equals("Boundary")) {
+            AudioSource.PlayClipAtPoint(missSound, transform.position);
+            GameManager.gm.decreaseLives();
+            Destroy(gameObject);
+        }
+    }
+}
+
